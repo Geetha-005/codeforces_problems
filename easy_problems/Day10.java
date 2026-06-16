07b6a9d1ad6e1101a9d715038647be7e7a991c98
Geetha-005 <chillageetha323@gmail.com>
2026-06-16 19:43:39 +0530

    day 10 problem solved

---
+++ easy_problems/Day10.java
@@ -0,0 +1,7 @@
+
+
+class Day10{
+    public static void main(String[] args) {
+        System.out.println("hello krishna");
+    }
+}
--- easy_problems/Demo.java
+++ easy_problems/Demo.java
@@ -2,6 +2,6 @@
 
 class Demo{
     public static void main(String[] args) {
-        System.out.println("hello krishna");
+        System.out.println("hello radhakrishna");
     }
 }
